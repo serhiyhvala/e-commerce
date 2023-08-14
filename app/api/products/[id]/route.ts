8d75b881e2dce70b3506e7dacf0a3f6215e04fa9
serhiyhvala <serhiyhvala.code@gmail.com>
2023-08-15 02:42:53 +0300

    Create route to update product

--- app/api/products/[id]/route.ts
+++ app/api/products/[id]/route.ts
@@ -1,6 +1,7 @@
 import {auth} from "@clerk/nextjs";
 import {NextResponse} from "next/server";
 import prismaDb from "@/configs/prisma";
+import {Product} from "@prisma/client";
 
 export async function GET(req: Request, {params}: {params: {id: string}}){
     try {
@@ -13,8 +14,33 @@ export async function GET(req: Request, {params}: {params: {id: string}}){
                 id: params.id
             }
         })
-
         return NextResponse.json(currentProduct)
+    } catch (error){
+        return new NextResponse("Internal server error", {status: 500})
+    }
+}
+
+export async function PATCH(req: Request, {params}: {params: {id: string}}){
+    try {
+        const {userId} = auth()
+        const {description, image, title, price}: Product = await req.json()
+        if (!userId) {
+            return new NextResponse("Unauthenticated", { status: 403 });
+        }
+
+        const updatedProduct = await prismaDb.product.updateMany({
+            where: {
+                id: params.id
+            },
+            data: {
+                title,
+                description,
+                image,
+                price: +price
+            }
+        })
+
+        return NextResponse.json(updatedProduct)
     } catch (error){
         return new NextResponse("Internal server error", {status: 500})
     }
