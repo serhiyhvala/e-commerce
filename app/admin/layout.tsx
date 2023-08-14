71c1a59e172d61709d5715f8c0792fba0ffcd169
serhiyhvala <serhiyhvala.code@gmail.com>
2023-08-15 02:40:35 +0300

    Rename Hook useAuth

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -2,7 +2,7 @@
 
 import {FC, ReactNode} from "react";
 import AdminHeader from "@/components/AdminHeader";
-import {useAuth} from "@/hooks";
+import {useAuth} from "@/hooks/useAuth";
 import {redirect} from "next/navigation";
 import Loading from "@/components/Loading";
 
