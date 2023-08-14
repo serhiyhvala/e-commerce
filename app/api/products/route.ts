3ec48d128c418c6dea989195fb4a163685ef7bb9
serhiyhvala <serhiyhvala.code@gmail.com>
2023-08-14 14:45:56 +0300

    Create Route to create new products

---
+++ app/api/products/route.ts
@@ -0,0 +1,29 @@
+import {auth} from "@clerk/nextjs";
+import prismaDb from "@/configs/prisma";
+import {NextResponse} from "next/server";
+import {Product} from "@prisma/client";
+
+export async function POST(req: Request){
+    try {
+        const {userId} = auth()
+        const {description, image, title, price}: Product = await req.json()
+        if(!userId){
+            return new NextResponse("Unauthorized", {status: 401})
+        }
+
+        const newProduct = await prismaDb.product.create({
+            data: {
+                description,
+                image,
+                price: +price,
+                title
+            }
+        })
+
+        return NextResponse.json(newProduct)
+
+    } catch(error){
+        console.log('PRODUCT_POST', error)
+        return new NextResponse("Internal Error")
+    }
+}
