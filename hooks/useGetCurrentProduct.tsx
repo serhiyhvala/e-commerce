a8f23da8698a4c348ea5835a75b164ab87fab866
serhiyhvala <serhiyhvala.code@gmail.com>
2023-08-15 02:42:10 +0300

    Create hook useGetCurrentProduct

---
+++ hooks/useGetCurrentProduct.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import {useEffect, useState} from "react";
+import axios from "axios";
+import {Product} from "@prisma/client";
+
+export const useGetCurrentProduct = (id: string) => {
+    const [isLoading, setIsLoading] = useState(true)
+    const [currentProduct, setCurrentProduct] = useState<Product>()
+    useEffect(() => {
+        const getCurrentProduct = async () => {
+            try {
+                const {data} = await axios.get<Product>(`/api/products/${id}`)
+                setIsLoading(false)
+                return data
+            } catch (error) {
+                console.error(error)
+            }
+        }
+        getCurrentProduct().then(data => setCurrentProduct(data))
+    }, [id])
+    return {currentProduct, isLoading, setIsLoading}
+}
