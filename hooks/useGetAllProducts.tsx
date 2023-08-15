301ed474fe9b34cb0f0c0e2996d6dcb086414bf0
serhiyhvala <serhiyhvala.code@gmail.com>
2023-08-15 20:38:13 +0300

    Update hooks

--- hooks/useGetAllProducts.tsx
+++ hooks/useGetAllProducts.tsx
@@ -1,24 +1,25 @@
-import {useEffect, useState} from "react";
-import {Product} from "@prisma/client";
+import { useEffect, useState } from "react";
+import { Product } from "@prisma/client";
 import axios from "axios";
+import { IProduct } from "@/types/product.types";
 
 export const useGetAllProducts = () => {
-    const [isLoading, setIsLoading] = useState(true)
-    const [products, setProducts] = useState<Product[]>([])
+  const [isLoading, setIsLoading] = useState(true);
+  const [products, setProducts] = useState<Product[]>([]);
 
-    useEffect(() => {
-        const getAllProducts = async() => {
-            try {
-                const {data} = await axios.get("/api/products")
-                setIsLoading(false)
-                return data
-            } catch (error){
-                console.error(error)
-            }
-        }
+  useEffect(() => {
+    const getAllProducts = async () => {
+      try {
+        const data = await fetch("/api/products", { cache: "no-store" });
+        setIsLoading(false);
+        return data.json();
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-        getAllProducts().then(data => setProducts(data))
-    }, [])
+    getAllProducts().then(data => setProducts(data));
+  }, []);
 
-    return {isLoading, products}
-}
+  return { isLoading, products };
+};
--- hooks/useGetCurrentProduct.tsx
+++ hooks/useGetCurrentProduct.tsx
@@ -1,23 +1,25 @@
-'use client'
+"use client";
 
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
-import {Product} from "@prisma/client";
+import { Product } from "@prisma/client";
 
 export const useGetCurrentProduct = (id: string) => {
-    const [isLoading, setIsLoading] = useState(true)
-    const [currentProduct, setCurrentProduct] = useState<Product>()
-    useEffect(() => {
-        const getCurrentProduct = async () => {
-            try {
-                const {data} = await axios.get<Product>(`/api/products/get/${id}`)
-                setIsLoading(false)
-                return data
-            } catch (error) {
-                console.error(error)
-            }
-        }
-        getCurrentProduct().then(data => setCurrentProduct(data))
-    }, [id])
-    return {currentProduct, isLoading, setIsLoading}
-}
+  const [isLoading, setIsLoading] = useState(true);
+  const [currentProduct, setCurrentProduct] = useState<Product>();
+  useEffect(() => {
+    const getCurrentProduct = async () => {
+      try {
+        const data = await fetch(`/api/products/get/${id}`, {
+          cache: "no-store",
+        });
+        setIsLoading(false);
+        return data.json();
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getCurrentProduct().then(data => setCurrentProduct(data));
+  }, [id]);
+  return { currentProduct, isLoading, setIsLoading };
+};
