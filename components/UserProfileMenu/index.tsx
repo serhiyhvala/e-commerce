00f3219379babe9aeb4a301be184f450e7645981
serhiyhvala <serhiyhvala.code@gmail.com>
2023-08-16 02:01:43 +0300

    Update userProfileMenu

--- components/UserProfileMenu/index.tsx
+++ components/UserProfileMenu/index.tsx
@@ -1,14 +1,16 @@
-'use state'
+'use client'
 
 import {DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger} from "@/components/ui/dropdown-menu";
-import {LayoutDashboard} from "lucide-react";
+import {LayoutDashboard, User} from "lucide-react";
 import Link from "next/link";
 import {SignOutButton, useUser} from "@clerk/nextjs";
 import {useState} from "react";
 import {Avatar, AvatarImage} from "@/components/ui/avatar";
+import {useAuth} from "@/hooks/useAuth";
 
 const UserProfileMenu = () => {
     const [isOpen, setIsOpen] = useState(false)
+    const {user: currentUser} = useAuth()
     const {user} = useUser()
     return (
         <DropdownMenu>
@@ -21,10 +23,18 @@ const UserProfileMenu = () => {
                 <DropdownMenuContent>
                     <DropdownMenuItem onClick={() => setIsOpen(false)}>
                         <Link href="/profile" className='w-full flex items-center gap-2'>
-                            <LayoutDashboard className='w-5 h-5'/>
+                            <User className='w-5 h-5'/>
                             <span>Profile</span>
                         </Link>
                     </DropdownMenuItem>
+                    {currentUser?.isAdmin && (
+                        <DropdownMenuItem onClick={() => setIsOpen(false)}>
+                            <Link href="/admin" className='w-full flex items-center gap-2'>
+                                <LayoutDashboard className='w-5 h-5'/>
+                                <span>Admin</span>
+                            </Link>
+                        </DropdownMenuItem>
+                    )}
                     <DropdownMenuItem onClick={() => setIsOpen(false)}>
                         <SignOutButton />
                     </DropdownMenuItem>
