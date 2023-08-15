1e255d5efe448a070d23da8c236b3c4ec8d60420
serhiyhvala <serhiyhvala.code@gmail.com>
2023-08-15 16:28:58 +0300

    Update User Mobile component

--- components/UserMobileMenu/index.tsx
+++ components/UserMobileMenu/index.tsx
@@ -1,22 +1,33 @@
 import {DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger} from "@/components/ui/dropdown-menu";
-import {MenuIcon} from "lucide-react";
+import {MenuIcon, User} from "lucide-react";
 import Link from "next/link";
+import {useAuth} from "@/hooks/useAuth";
 
 const UserMobileMenu = () => {
+    const {userId} = useAuth()
     return (
         <DropdownMenu>
             <DropdownMenuTrigger><MenuIcon className='block sm:hidden'/></DropdownMenuTrigger>
             <DropdownMenuContent>
-                <DropdownMenuItem>
-                    <Link href="/sign-in" className='w-full flex items-center gap-2'>
-                        <span>Sign In</span>
+                {!userId ? (
+                    <>
+                        <DropdownMenuItem>
+                            <Link href="/sign-in" className='w-full flex items-center gap-2'>
+                                <span>Sign In</span>
+                            </Link>
+                        </DropdownMenuItem>
+                        <DropdownMenuItem>
+                            <Link href="/sign-up" className='w-full flex items-center gap-2'>
+                                <span>Sign Up</span>
+                            </Link>
+                        </DropdownMenuItem>
+                    </>
+                ) : <DropdownMenuItem>
+                    <Link href='/profile' className='w-full flex items-center gap-2'>
+                        <User />
+                        <span>Profile</span>
                     </Link>
-                </DropdownMenuItem>
-                <DropdownMenuItem>
-                    <Link href="/sign-up" className='w-full flex items-center gap-2'>
-                        <span>Sign Up</span>
-                    </Link>
-                </DropdownMenuItem>
+                </DropdownMenuItem>}
             </DropdownMenuContent>
         </DropdownMenu>
     );
