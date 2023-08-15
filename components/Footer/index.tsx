bfe9fe1cb9b5f0d454f53d46e883b81784b8174a
serhiyhvala <serhiyhvala.code@gmail.com>
2023-08-15 14:39:26 +0300

    Create Footer component

---
+++ components/Footer/index.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer className='flex w-full border-t-2 item-center justify-center p-5'>
+            <span className='font-light text-sm text-gray-500'>© 2023 Store, Inc. All rights reserved.</span>
+        </footer>
+    );
+};
+
+export default Footer;
