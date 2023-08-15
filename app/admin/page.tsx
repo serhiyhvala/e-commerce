d969cde43f3aa04d1a2a68752e011ad73b24dd89
serhiyhvala <serhiyhvala.code@gmail.com>
2023-08-15 18:08:08 +0300

    Update global styles and remove User button

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,12 +1,8 @@
 'use client'
 
-import {UserButton, useUser} from "@clerk/nextjs";
-
 const Admin = () => {
-  const {user} = useUser()
   return <div>
-    <UserButton />
-    <h2>{user?.id}</h2>
+
   </div>;
 };
 
