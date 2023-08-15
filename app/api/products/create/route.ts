b884b4e648795214212377332a884e970d4cb7f6
serhiyhvala <serhiyhvala.code@gmail.com>
2023-08-15 14:35:47 +0300

    Create new api route to post new product

---
+++ app/api/products/create/route.ts
@@ -0,0 +1,29 @@
+import {auth} from "@clerk/nextjs";
+import {NextResponse} from "next/server";
+import {IProduct} from "@/types/product.types";
+import prismaDb from "@/configs/prisma";
+
+export async function POST(req: Request){
+    try {
+        const {userId} = auth()
+        if(!userId){
+            return new NextResponse("Unauthorized", {status: 401})
+        }
+        const {description, image, title, price}: IProduct = await req.json()
+
+        const newProduct = await prismaDb.product.create({
+            data: {
+                description,
+                image,
+                title,
+                price: +price,
+            }
+        })
+
+        return NextResponse.json(newProduct)
+
+    } catch(error){
+        console.log('PRODUCT_POST', error)
+        return new NextResponse("Internal Error")
+    }
+}
--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,38 +1,13 @@
 import {auth} from "@clerk/nextjs";
 import prismaDb from "@/configs/prisma";
 import {NextResponse} from "next/server";
-import {Product} from "@prisma/client";
+import {IProduct} from "@/types/product.types";
 
 export async function GET(req: Request){
     try {
         const allProducts = await prismaDb.product.findMany({})
 
         return NextResponse.json(allProducts)
-
-    } catch(error){
-        console.log('PRODUCT_POST', error)
-        return new NextResponse("Internal Error")
-    }
-}
-
-export async function POST(req: Request){
-    try {
-        const {userId} = auth()
-        const {description, image, title, price}: Product = await req.json()
-        if(!userId){
-            return new NextResponse("Unauthorized", {status: 401})
-        }
-
-        const newProduct = await prismaDb.product.create({
-            data: {
-                description,
-                image,
-                price: +price,
-                title
-            }
-        })
-
-        return NextResponse.json(newProduct)
 
     } catch(error){
         console.log('PRODUCT_POST', error)
