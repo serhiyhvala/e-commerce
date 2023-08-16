eca031f2bf56aa403ef5e9a86787ae084a5bfb96
serhiyhvala <serhiyhvala.code@gmail.com>
2023-08-16 16:38:37 +0300

    Update Related Items component

--- components/RelatedItems/index.tsx
+++ components/RelatedItems/index.tsx
@@ -5,6 +5,8 @@ import ProductCard from "@/components/ProductCard";
 import {Expand, ShoppingCart} from "lucide-react";
 import {useGetAllProducts} from "@/hooks/useGetAllProducts";
 import Skeleton from "@/components/Skeleton";
+import Link from "next/link";
+import {useStore} from "@/store/store";
 
 interface IRelatedItemsProps {
     currentProductId: string
@@ -12,6 +14,7 @@ interface IRelatedItemsProps {
 
 const RelatedItems :FC<IRelatedItemsProps>= ({currentProductId}) => {
     const {products, isLoading} = useGetAllProducts()
+    const {handleAddItemToCart} = useStore()
 
     const relatedItems = products.filter(item => item.id !== currentProductId).slice(0, 3)
     return (
@@ -26,11 +29,12 @@ const RelatedItems :FC<IRelatedItemsProps>= ({currentProductId}) => {
                 {relatedItems.map(item => (
                     <ProductCard key={item.id} link={`/products/${item.id}`} {...item}>
                         <>
-                            <span className='rounded-full bg-black p-3 flex justify-center'>
+                            <Link href={`/products/${item.id}`}
+                                  className='rounded-full bg-black p-3 flex justify-center'>
                                 <Expand className='text-white'/>
-                            </span>
-                            <span className='rounded-full bg-black p-3 flex justify-center'>
-                                <ShoppingCart className='text-white'/>
+                            </Link>
+                            <span className='rounded-full bg-black p-3 flex justify-center cursor-pointer'>
+                                <ShoppingCart className='text-white' onClick={() => handleAddItemToCart(item)}/>
                             </span>
                         </>
                     </ProductCard>
