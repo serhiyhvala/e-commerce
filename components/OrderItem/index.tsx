f8a78e842d5c8614d5eb08db6061f40eeac2401d
serhiyhvala <serhiyhvala.code@gmail.com>
2023-08-16 16:21:33 +0300

    Create OrderItem component and create Admin Orders Page

---
+++ components/OrderItem/index.tsx
@@ -0,0 +1,48 @@
+import Image from "next/image";
+import {Button} from "@/components/ui/button";
+import Link from "next/link";
+import {Expand} from "lucide-react";
+import {FC} from "react";
+import {IOrder} from "@/types/user.types";
+
+const OrderItem:FC<IOrder> = ({products,fullName, address, email,totalPrice  }) => {
+    return (
+        <div className='flex justify-center sm:justify-start flex-wrap bg-gray-100 rounded-xl p-2 gap-5'>
+            <div className="flex flex-col gap-2">
+                <span className='text-2xl font-bold pb-2 border-b-2'>Products</span>
+                {products.map(item => (
+                    <div className="flex flex-col gap-2 relative" key={item.id}>
+                        <Image src={item.image} alt={item.title} width={200} height={200}
+                               className='rounded-xl'/>
+                        <Button asChild>
+                            <Link href={`/products/${item.id}`} className='rounded-full bg-black p-3 flex justify-center absolute top-2 right-2'>
+                                <Expand className='text-white'/>
+                            </Link>
+                        </Button>
+                    </div>
+                ))}
+            </div>
+            <div className="flex flex-col gap-2">
+                <span className='text-2xl font-bold pb-2 border-b-2'>Details</span>
+                <div className='flex items-center gap-2'>
+                    <span className='font-bold'>Full Name:</span>
+                    <span>{fullName}</span>
+                </div>
+                <div className='flex items-center gap-2'>
+                    <span className='font-bold'>Email:</span>
+                    <span>{email}</span>
+                </div>
+                <div className='flex items-center gap-2'>
+                    <span className='font-bold'>Address:</span>
+                    <span>{address}</span>
+                </div>
+                <div className='flex items-center gap-2'>
+                    <span className='font-bold'>Total Price:</span>
+                    <span>${totalPrice}</span>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default OrderItem;
--- components/Orders/index.tsx
+++ components/Orders/index.tsx
@@ -1,8 +1,5 @@
 import {useAuth} from "@/hooks/useAuth";
-import Image from "next/image";
-import {Button} from "@/components/ui/button";
-import Link from "next/link";
-import {Expand} from "lucide-react";
+import OrderItem from "@/components/OrderItem";
 
 const Orders = () => {
     const {user} = useAuth()
@@ -12,41 +9,7 @@ const Orders = () => {
             <div className="flex items-center gap-5 flex-wrap">
                 {user?.orders.length ? (
                     user.orders.map(item => (
-                        <div className='flex justify-center sm:justify-start flex-wrap bg-gray-100 rounded-xl p-2 gap-5' key={item.id}>
-                            <div className="flex flex-col gap-2">
-                                <span className='text-2xl font-bold pb-2 border-b-2'>Products</span>
-                                {item.products.map(item => (
-                                    <div className="flex flex-col gap-2 relative" key={item.id}>
-                                        <Image src={item.image} alt={item.title} width={200} height={200}
-                                               className='rounded-xl'/>
-                                        <Button asChild>
-                                            <Link href={`/products/${item.id}`} className='rounded-full bg-black p-3 flex justify-center absolute top-2 right-2'>
-                                                <Expand className='text-white'/>
-                                            </Link>
-                                        </Button>
-                                    </div>
-                                ))}
-                            </div>
-                            <div className="flex flex-col gap-2">
-                                <span className='text-2xl font-bold pb-2 border-b-2'>Details</span>
-                                <div className='flex items-center gap-2'>
-                                    <span className='font-bold'>Full Name:</span>
-                                    <span>{item.fullName}</span>
-                                </div>
-                                <div className='flex items-center gap-2'>
-                                    <span className='font-bold'>Email:</span>
-                                    <span>{item.email}</span>
-                                </div>
-                                <div className='flex items-center gap-2'>
-                                    <span className='font-bold'>Address:</span>
-                                    <span>{item.address}</span>
-                                </div>
-                                <div className='flex items-center gap-2'>
-                                    <span className='font-bold'>Total Price:</span>
-                                    <span>${item.totalPrice}</span>
-                                </div>
-                            </div>
-                        </div>
+                        <OrderItem key={item.id} {...item}/>
                     ))
                 ) : <span className='text-5xl font-bold'>You don&apos;t have any liked products</span>}
             </div>
