b009e141649cbaae62eb1e3153e5ea53edce971a
serhiyhvala <serhiyhvala.code@gmail.com>
2023-08-17 11:58:32 +0300

    Create FAQ component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -6,38 +6,35 @@ import {Expand, ShoppingCart} from "lucide-react";
 import Skeleton from "@/components/Skeleton";
 import Link from "next/link";
 import {useStore} from "@/store/store";
+import {useEffect, useState} from "react";
+import RelatedProducts from "@/components/FeaturedProducts";
+import FeaturedProducts from "@/components/FeaturedProducts";
+import {Accordion, AccordionContent, AccordionItem, AccordionTrigger} from "@/components/ui/accordion";
+import FAQ from "@/components/FAQ";
 
 export default function Home() {
-    const {isLoading, products} = useGetAllProducts()
-    const {handleAddItemToCart} = useStore()
+    const [isMounted, setIsMounted] = useState(false)
 
-    return <section className='flex flex-1 flex-col justify-center w-full max-w-6xl mx-auto px-3 py-5 gap-6'>
+    useEffect(() => {
+        setIsMounted(true)
+    }, [])
+
+    if (!isMounted) {
+        return null
+    }
+
+    return <section className='flex flex-1 flex-col justify-center w-full max-w-6xl mx-auto px-3 py-5 gap-10'>
         <div
             className="w-full h-[250px] sm:h-[450px]  bg-gradient-to-r from-cyan-400 to-blue-800 rounded-xl flex items-center justify-center">
             <span className='font-bold text-2xl sm:text-5xl text-white'>Explore Our Products!</span>
         </div>
-        <div className="flex flex-col gap-3">
-            <span className='font-bold text-3xl'>Featured Products</span>
-            <div className="flex items-center justify-center flex-wrap gap-16">
-                {isLoading && (
-                    new Array(6).fill(Math.random()).map(item => (
-                        <Skeleton key={item}/>
-                    ))
-                )}
-                {products.map(item => (
-                    <ProductCard key={item.id} {...item} link={`/products/${item.id}`} likeButton>
-                        <>
-                            <Link href={`/products/${item.id}`}
-                                  className='rounded-full bg-black p-3 flex justify-center'>
-                                <Expand className='text-white'/>
-                            </Link>
-                            <span className='rounded-full bg-black p-3 flex justify-center cursor-pointer'>
-                                <ShoppingCart className='text-white' onClick={() => handleAddItemToCart(item)}/>
-                            </span>
-                        </>
-                    </ProductCard>
-                ))}
-            </div>
+        <div className="flex flex-col gap-7">
+            <span className='font-bold text-3xl pb-3 border-b-2'>Featured Products</span>
+            <FeaturedProducts />
+        </div>
+        <div className="flex flex-col">
+            <span className='font-bold text-3xl'>FAQ</span>
+            <FAQ />
         </div>
     </section>;
 }
