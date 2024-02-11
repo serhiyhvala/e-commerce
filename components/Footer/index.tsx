572e5258769948a935189c4f5bd47462345dcea7
Sergiy Khvala <sergiy.bootcamp@macpaw.com>
2024-02-11 14:30:42 +0200

    feat: remove unused components and update date in footer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="flex w-full border-t-2 item-center justify-center p-5">
       <span className="font-light text-sm text-gray-500">
-        © 2023 Store, Inc. All rights reserved.
+        © {new Date().getFullYear()} Store, Inc. All rights reserved.
       </span>
     </footer>
   );
